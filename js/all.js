fb410c86d70ea9451e3c214c0468c8c672ba391d
highmetal <haineco1199air@gmail.com>
2026-04-25 16:52:27 +0900

    ヘッダーの時計作成

--- js/all.js
+++ js/all.js
@@ -1,5 +1,31 @@
 "use strict";
 
+//時計
+const clockElement = document.getElementById("clock");
+function clock() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minut = date.getMinutes();
+  const sec = date.getSeconds();
+  //文字列作成
+  clockElement.textContent = `
+  ${year}/
+  ${formatTwoDigits(month)}/
+  ${formatTwoDigits(day)} 
+  ${formatTwoDigits(hour)}:
+  ${formatTwoDigits(minut)}:
+  ${formatTwoDigits(sec)}
+  `;
+}
+//2桁文字列型変換
+function formatTwoDigits(value) {
+  return String(value).padStart(2, "0");
+}
+
+//サイト内検索処理
 const searchInput = document.getElementById("searchInput");
 const searchButton = document.getElementById("searchButton");
 
