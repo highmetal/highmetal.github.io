908f91f2b04d4a0807eff9d2cfd593a4f5fa6b98
highmetal <haineco1199air@gmail.com>
2026-04-25 15:25:45 +0900

    サイト内検索機能を追加

---
+++ js/search.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const searchResult = document.getElementById("searchResult");
+
+let pages = [];
+
+//検索文字列取得
+const parms = new URLSearchParams(window.location.search);
+const word = parms.get("q") || "";
+
+//検索結果のjson読み込み
+fetch("searchList.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    pages = data;
+    searchRender();
+  });
+
+//検索結果表示
+function searchRender() {
+  let hitCount = 0;
+  pages.forEach(function (page) {
+    //ヒットしたなら表示
+    if (page.tags.includes(word.toLowerCase())) {
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("pageLink");
+
+      const newA = document.createElement("a");
+      newA.href = page.url;
+      newA.textContent = page.title;
+      newA.target = "_blank";
+
+      newDiv.appendChild(newA);
+      searchResult.appendChild(newDiv);
+      hitCount++;
+    }
+  });
+  //ヒットが一つもないなら
+  if (hitCount === 0) {
+    const newP = document.createElement("p");
+    newP.textContent = "検索結果はありません。";
+    searchResult.appendChild(newP);
+  }
+}
