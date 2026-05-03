18274f1c6b755d10d1d77e8c2490ca9c8ade4287
highmetal <haineco1199air@gmail.com>
2026-05-03 21:16:01 +0900

    自己紹介js修正

--- src/aboutMe/topBack.js
+++ src/aboutMe/topBack.js
@@ -1,17 +1,18 @@
 "use strict";
-    const topBack = document.getElementById("topBack");
 
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > 100) {
-        topBack.style.visibility = "visible";
-      } else {
-        topBack.style.visibility = "hidden";
-      }
-    });
+const topBack = document.getElementById("topBack");
 
-    $(function () {
-      var pagescroll = $("#topBack").click(function () {
-        $("body, html").animate({ scrollTop: 0 }, 1000);
-        return false;
-      });
-    });
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    topBack.style.visibility = "visible";
+  } else {
+    topBack.style.visibility = "hidden";
+  }
+});
+
+$(function () {
+  var pagescroll = $("#topBack").click(function () {
+    $("body, html").animate({ scrollTop: 0 }, 1000);
+    return false;
+  });
+});
